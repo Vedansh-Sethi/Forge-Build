a7b3805042f8c7f853666c5453c1439b8a248857
Vedansh <vedansh25sethi@gmail.com>
2026-01-10 10:50:39 +0000

    [Week1] : Simple Storage Contract

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Forge Build
 
 ## About
+
 A 4 week workshop for teaching basics of web3, especially Blockchain and Smart Contracts
 
 ## Week 1
+
 - Task 1 : Creating Basic Skeleton for a Personal Website using HTML
 - Task 2 : Styling the website using CSS
 - Task 3 : Basic JS task -
-    - Adding an interactive counter
-    - Form that shows submission 
-    - Toggling Themes
+  - Adding an interactive counter
+  - Form that shows submission
+  - Toggling Themes
+- Task 4 : Creating a SimpleStorage contract
---
+++ solidity/contracts/SimpleStorage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleStorage {
+    struct Person {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    Person[] public listOfAllPeople;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfAllPeople.push(
+            Person({favoriteNumber: _favoriteNumber, name: _name})
+        );
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function seePerson(string memory _name) public view returns (uint256) {
+        return nameToFavoriteNumber[_name];
+    }
+}
